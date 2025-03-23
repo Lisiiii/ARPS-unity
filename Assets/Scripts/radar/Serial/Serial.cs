25dbf8512d04cb560bb439a4fe7343d7749460f9
Lrinaus <lisiyao20041017@outlook.com>
2025-03-23 21:34:26 +0800

    Add serial support (part2)

--- Assets/Scripts/radar/Serial/Serial.cs
+++ Assets/Scripts/radar/Serial/Serial.cs
@@ -5,135 +5,139 @@ using System.IO.Ports;
 using System.Threading;
 using UnityEngine;
 
-public class Serial : MonoBehaviour
+namespace radar.serial
 {
-    string[] ports;
-    SerialPort current_sp;
-    public byte[] receivedData = new byte[1024];
-    void Start()
+    public class Serial : MonoBehaviour
     {
-        Debug.Log("--- Serial Init ---");
-        ports = ScanPorts();
-        Debug.Log("Scaned ports count: " + ports.Length);
-        foreach (string port in ports)
+        string[] ports;
+        SerialPort current_sp;
+        public byte[] receivedData = new byte[1024];
+        void Start()
         {
-            if (port.Contains("COM1"))
+            Debug.Log("--- Serial Init ---");
+            ports = ScanPorts();
+            Debug.Log("Scaned ports count: " + ports.Length);
+            foreach (string port in ports)
             {
-                if (ConnectToPort(ref current_sp, port, 9600, Parity.None, 8, StopBits.One))
+                if (port.Contains("COM1"))
                 {
-                    Debug.Log("Connect to " + port);
-                    StartCoroutine(SendDataToSerialPort());
-                }
-            }
-        }
-    }
-    void Update()
-    {
-    }
-
-    // send data to serial port every 2 seconds (DEBUG)
-    IEnumerator SendDataToSerialPort()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(2);
-            SendData(ref current_sp, "Hello");
-            Debug.Log("Send data to " + current_sp.PortName);
-        }
-    }
-
-    public string[] ScanPorts()
-    {
-        string[] portList = SerialPort.GetPortNames();
-        return portList;
-    }
-
-    public bool ConnectToPort(ref SerialPort sp, string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
-    {
-        try
-        {
-            sp = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
-            sp.Open();
-            Thread thread = new Thread(new ThreadStart(DataReceivedHandler));
-            thread.Start();
-            return true;
-        }
-        catch (Exception ex)
-        {
-            Debug.Log(ex);
-            return false;
-        }
-    }
-
-    private void DataReceivedHandler()
-    {
-        while (true)
-        {
-            if (current_sp.IsOpen)
-            {
-                int count = current_sp.BytesToRead;
-                if (count > 0)
-                {
-                    byte[] readBuffer = new byte[count];
-                    try
+                    if (ConnectToPort(ref current_sp, port, 9600, Parity.None, 8, StopBits.One))
                     {
-                        current_sp.Read(readBuffer, 0, count);
-                        // StringBuilder sb = new StringBuilder();
-                        // for (int i = 0; i < readBuffer.Length; i++)
-                        // {
-                        //     sb.AppendFormat("{0:x2}" + "", readBuffer[i]);
-                        // }
-                        // Debug.Log(sb.ToString());
-                        receivedData = readBuffer;
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.Log(ex.Message);
+                        Debug.Log("Connect to " + port);
+                        StartCoroutine(SendDataToSerialPort());
                     }
                 }
             }
-            Thread.Sleep(10);
         }
-    }
+        void Update()
+        {
+        }
 
-    private void SendData(ref SerialPort sp, string _info)
-    {
-        try
+        // send data to serial port every 2 seconds (DEBUG)
+        IEnumerator SendDataToSerialPort()
         {
-            if (sp.IsOpen)
+            while (true)
             {
-                sp.WriteLine(_info);
-            }
-            else
-            {
-                sp.Open();
-                sp.WriteLine(_info);
+                yield return new WaitForSeconds(2);
+                SendData(ref current_sp, "Hello");
+                Debug.Log("Send data to " + current_sp.PortName);
             }
         }
-        catch (Exception ex)
-        {
-            Debug.Log(ex);
-        }
-    }
-    private void SendData(ref SerialPort sp, byte[] send, int offSet, int count)
-    {
-        try
-        {
-            if (sp.IsOpen)
-            {
-                sp.Write(send, offSet, count);
-            }
-            else
-            {
-                sp.Open();
-                sp.Write(send, offSet, count);
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.Log(ex);
-        }
-    }
 
+        public string[] ScanPorts()
+        {
+            string[] portList = SerialPort.GetPortNames();
+            return portList;
+        }
+
+        public bool ConnectToPort(ref SerialPort sp, string portName, int baudRate, Parity parity, int dataBits, StopBits stopBits)
+        {
+            try
+            {
+                sp = new SerialPort(portName, baudRate, parity, dataBits, stopBits);
+                sp.Open();
+                Thread thread = new Thread(new ThreadStart(DataReceivedHandler));
+                thread.Start();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex);
+                return false;
+            }
+        }
+
+        private void DataReceivedHandler()
+        {
+            while (true)
+            {
+                if (current_sp.IsOpen)
+                {
+                    int count = current_sp.BytesToRead;
+                    if (count > 0)
+                    {
+                        byte[] readBuffer = new byte[count];
+                        try
+                        {
+                            current_sp.Read(readBuffer, 0, count);
+                            // StringBuilder sb = new StringBuilder();
+                            // for (int i = 0; i < readBuffer.Length; i++)
+                            // {
+                            //     sb.AppendFormat("{0:x2}" + "", readBuffer[i]);
+                            // }
+                            // Debug.Log(sb.ToString());
+                            receivedData = readBuffer;
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.Log(ex.Message);
+                        }
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private void SendData(ref SerialPort sp, string _info)
+        {
+            try
+            {
+                if (sp.IsOpen)
+                {
+                    sp.WriteLine(_info);
+                }
+                else
+                {
+                    sp.Open();
+                    sp.WriteLine(_info);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex);
+            }
+        }
+        private void SendData(ref SerialPort sp, byte[] send, int offSet, int count)
+        {
+            try
+            {
+                if (sp.IsOpen)
+                {
+                    sp.Write(send, offSet, count);
+                }
+                else
+                {
+                    sp.Open();
+                    sp.Write(send, offSet, count);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex);
+            }
+        }
+
+
+    }
 
 }
